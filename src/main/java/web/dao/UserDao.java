a0d5c1e9e575a137ec5a589067e3d8b33f874219
StasAzin <114102778+StasAzin@users.noreply.github.com>
2023-03-13 01:06:49 +0500

    Add files via upload

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,19 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.models.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    void addUser(User user);
+
+    void updateUser(int id, User user);
+
+    void deleteUser(int id);
+
+    User getUserById(int id);
+
+    List<User> getUsers();
+}
---
+++ src/main/java/web/dao/UserDaoImp.java
@@ -0,0 +1,49 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import web.models.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Component
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void addUser(User user) {
+        em.persist(user);
+    }
+
+    @Override
+    public void updateUser(int id, User user) {
+        em.merge(user);
+    }
+
+    @Override
+    public void deleteUser(int id) {
+        em.remove(getUserById(id));
+    }
+
+    @Override
+    public User getUserById(int id) {
+        TypedQuery<User> q =  em.createQuery(
+                "select u from User u where u.id = :id", User.class
+        );
+        q.setParameter("id", id);
+        return q.getResultList().stream().findAny().orElse(null);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return em.createQuery(
+                "select u from User u", User.class
+        ).getResultList();
+    }
+}
